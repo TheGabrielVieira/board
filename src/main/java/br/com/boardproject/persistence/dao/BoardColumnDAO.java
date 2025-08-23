7afd0b847a4233fafc0274a95acb04426ff7e64f
Gabriel <50024453+TheGabrielVieira@users.noreply.github.com>
2025-08-23 15:11:40 -0300

    feat: creating-cards-and-testing-read-operations

--- src/main/java/br/com/boardproject/persistence/dao/BoardColumnDAO.java
+++ src/main/java/br/com/boardproject/persistence/dao/BoardColumnDAO.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static br.com.boardproject.persistence.entity.BoardColumnKindEnum.findByName;
+import static java.util.Objects.isNull;
 
 @RequiredArgsConstructor
 public class BoardColumnDAO {
@@ -60,12 +61,12 @@ public class BoardColumnDAO {
                 SELECT bc.id,
                        bc.name,
                        bc.kind,
-                       COUNT(SELECT c.id
+                       (SELECT COUNT(c.id)
                                FROM CARDS c
                               WHERE c.board_column_id = bc.id) cards_amount
                   FROM BOARDS_COLUMNS bc
                  WHERE board_id = ?
-                 ORDER BY `order`
+                 ORDER BY `order`;
                 """;
         try(var statement = connection.prepareStatement(sql)){
             statement.setLong(1, boardId);
@@ -93,7 +94,7 @@ public class BoardColumnDAO {
                c.title,
                c.description
           FROM BOARDS_COLUMNS bc
-         INNER JOIN CARDS c
+          LEFT JOIN CARDS c
             ON c.board_column_id = bc.id
          WHERE bc.id = ?;
         """;
@@ -107,12 +108,16 @@ public class BoardColumnDAO {
                 entity.setName(resultSet.getString("name"));
                 do {
                     var card = new CardEntity();
+                    if (isNull(resultSet.getString("c.title"))){
+                        break;
+                    }
                     card.setId(resultSet.getLong("c.di"));
                     card.setTitle(resultSet.getString("c.title"));
                     card.setDescription(resultSet.getString("c.description"));
                     entity.getCards().add(card);
 
                 }while (resultSet.next());
+                return Optional.of(entity);
             }
             return Optional.empty();
         }
