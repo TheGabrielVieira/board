3cda436ae75cffb7a118fa8331c2cb1211e8240f
Gabriel <50024453+TheGabrielVieira@users.noreply.github.com>
2025-08-23 22:15:06 -0300

    feat: add-a-method-to-cancel-a-card

--- src/main/java/br/com/boardproject/service/CardService.java
+++ src/main/java/br/com/boardproject/service/CardService.java
@@ -51,8 +51,38 @@ public class CardService {
             }
             var nextColumn = boardColumnsInfo.stream()
                     .filter(bc -> bc.order() == currentColumn.order() + 1)
-                    .findFirst().orElseThrow();
+                    .findFirst().orElseThrow(() -> new IllegalArgumentException("o card esta cancelado"));
             dao.moveToColumn(nextColumn.id(), cardId);
+            connection.commit();
+        } catch (SQLException ex){
+            connection.rollback();
+            throw ex;
+        }
+    }
+
+    public void cancel(final Long cardId, final Long cancelColumnId,
+                       final List<BoardColumnInfoDTO> boardColumnsInfo) throws SQLException{
+        try {
+            var dao = new CardDAO(connection);
+            var optional = dao.findById(cardId);
+            var dto = optional.orElseThrow(
+                    () -> new EntityNotFoundException("O card id %s nao foi encontrado".formatted(cardId))
+            );
+            if (dto.blocked()){
+                var message = "O card %s esta bloqueado, e ncessario desbloquea-lo para mover".formatted(cardId);
+                throw new CardBlockedException(message);
+            }
+            var currentColumn = boardColumnsInfo.stream()
+                    .filter(bc -> bc.id().equals(dto.columnId()))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("O card informado pertence a outro board"));
+            if (currentColumn.kind().equals(FINAL)){
+                throw new CardFinishedException("O card já foi finalizado");
+            }
+            boardColumnsInfo.stream()
+                    .filter(bc -> bc.order() == currentColumn.order() + 1)
+                    .findFirst().orElseThrow(() -> new IllegalArgumentException("O card esta cancelado"));
+            dao.moveToColumn(cancelColumnId, cardId);
             connection.commit();
         } catch (SQLException ex){
             connection.rollback();
--- src/main/java/br/com/boardproject/ui/BoardMenu.java
+++ src/main/java/br/com/boardproject/ui/BoardMenu.java
@@ -71,7 +71,7 @@ public class BoardMenu {
     }
 
     private void moveCardToNextColumn() throws SQLException{
-        System.out.println("Informe o id do card que deseja mover para a proxima couna");
+        System.out.println("Informe o id do card que deseja mover para a proxima coluna");
         var cardId = scanner.nextLong();
         var boardColumnsInfo = entity.getBoardColumns().stream()
                 .map(bc -> new BoardColumnInfoDTO(bc.getId(), bc.getOrder(), bc.getKind()))
@@ -89,7 +89,18 @@ public class BoardMenu {
     private void unblockCard() {
     }
 
-    private void cancelCard() {
+    private void cancelCard() throws SQLException{
+        System.out.println("Informe o id do card que deseja mover para a proxima coluna de cancelamento");
+        var cardId = scanner.nextLong();
+        var cancelColumn = entity.getCancelColumn();
+        var boardColumnsInfo = entity.getBoardColumns().stream()
+                .map(bc -> new BoardColumnInfoDTO(bc.getId(), bc.getOrder(), bc.getKind()))
+                .toList();
+        try(var connection = getConnection()){
+            new CardService(connection).cancel(cardId, cancelColumn.getId(), boardColumnsInfo);
+        } catch (RuntimeException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 
     private void showBoard() throws SQLException {
